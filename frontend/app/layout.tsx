56f595c1e99fef5a6b3df1d00480c07f8edab1a0
technicalclipper <technicalclipper@gmail.com>
2026-05-22 16:08:46 +0530

    feat: replace Geist font with Public Sans in layout and remove unused Geist_Mono font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 import RetroGridBackground from "@/components/ui/RetroGridBackground";
 
-const geistSans = Geist({
+const geistSans = Public_Sans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "FANVAS - Democratizing Fan Art",
@@ -26,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} antialiased`}>
         <RetroGridBackground />
         {children}
       </body>
