8c515f17cd58546c243ab31da5651bc43d2ae775
technicalclipper <technicalclipper@gmail.com>
2026-05-28 21:02:40 +0530

    fix: database

--- frontend/lib/supabaseConfig.ts
+++ frontend/lib/supabaseConfig.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://rqbbpycsofxiiwuabkom.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxYmJweWNzb2Z4aWl3dWFia29tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5OTA4NDMsImV4cCI6MjA2MTU2Njg0M30.RZtdoxOZfvuVOph-Qy3E6io2RfEerddwKylBS3th59w";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://cjickdvjcfcfkbfibunn.supabase.co";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqaWNrZHZqY2ZjZmtiZmlidW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NjY2NjMsImV4cCI6MjA5NTU0MjY2M30.X6hxtA04jzkjPBnTipbPHpOIPzMtQGrqzmHZut-D56Q";
 
 if (!supabaseUrl || !supabaseKey) {
   console.error('Missing Supabase environment variables');
